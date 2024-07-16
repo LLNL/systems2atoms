a1409afbad84c2d5ee4c331e47d8f0f944293fd9
Ethan Sunshine <esunshin@andrew.cmu.edu>
2024-07-16 15:39:18 -0700

    Switch to pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="systems2atoms",
-    version="0.0.0",
-    description="Models built for cross-scale optimization for the hydrogen economy",
-    url="https://github.com/LLNL/systems2atoms",
-    include_package_data=True,
-)
