Fix calculation of current_loss in _classes.py

systems2atoms/hyperplanetree/hyperplanetree/_classes.py
@@ -654,7 +654,7 @@ class _LinearTree(BaseDecisionTree):
 
                 if len(queue) > 0:
                     if self.early_stop_loss > -torch.inf:
-                        current_loss = torch.sum([node.loss * node.n_samples for node in self._nodes if node.children is None]) / len(X)
+                        current_loss = sum([node.loss * node.n_samples for node in self._nodes.values() if node.children is None]) / len(X)
                         print(current_loss)
                         if current_loss < self.early_stop_loss:
                             break